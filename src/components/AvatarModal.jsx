ecf513e365764632c2847122e5d6468b563c0be0
Yoon Ji Jeong <candace@Yoons-MacBook-Air.local>
2021-12-11 22:24:46 -0800

    add styling to avatarModal

--- src/components/AvatarModal.jsx
+++ src/components/AvatarModal.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import { useFilePicker } from 'use-file-picker';
 import styled from 'styled-components';
 import Modal from 'styled-react-modal';
 import { useSelector, useDispatch } from 'react-redux';
@@ -6,6 +7,16 @@ import { changeImage, changeUsername } from '../slices/userSlice';
 
 
 export function AvatarModal(props) {
+
+    const [openFileSelector, { filesContent, loading }] = useFilePicker({
+        multiple: false,
+        accept: 'image/*',
+        readAs: 'DataURL'
+    });
+
+    useEffect(() => {
+        console.log(openFileSelector)
+    },[])
 
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user);
@@ -15,15 +26,11 @@ export function AvatarModal(props) {
     const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
-        console.log(image);
-    },[image])
-
-    const handleImage = (e) => {
-        //console.log('hi')
-        //console.log('changing the image')
-        //console.log(e.target.files[0].name);
-        dispatch(changeImage(e.target.files[0].name));
-    }
+        if(filesContent.length > 0) { 
+            console.log(filesContent[0].content)
+            dispatch(changeImage(filesContent[0].content));            
+        }
+    },[filesContent])
 
     const toggleModal = (event) => {
         setIsOpen(!isOpen);
@@ -32,39 +39,89 @@ export function AvatarModal(props) {
     const handleUsername = (e) => {
         console.log(`Your new username is ${username}`);
         dispatch(changeUsername(username));
+        toggleModal(null);
     }
 
     return(
         <div>
-            <Image onClick={toggleModal} style={{backgroundImage: `url(${user.image})`}}></Image>
-            <StyledModal isOpen = {isOpen}  onBackgroundClick={toggleModal} onEscapeKeydown={toggleModal}>
-                <Image style={{backgroundImage: `url(${user.image})`}}></Image>
-                <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
-                <input type="file"
-                    id="avatar" name="avatar"
-                    accept="image/png, image/jpeg" onChange={handleImage}></input>
-                <button type="button" onClick={handleUsername}>Submit</button>
+            <Image onClick={toggleModal} style={{backgroundImage: `url(${user.image})`, width: '50px', height: '50px'}}></Image>
+            <StyledModal isOpen = {isOpen} onBackgroundClick={toggleModal} onEscapeKeydown={toggleModal}>
+                <LeftModal>
+                    <ImageModal style={{backgroundImage: `url(${user.image})`}}></ImageModal>
+                    <EditButton onClick={() => openFileSelector()}>Edit</EditButton>
+                </LeftModal>
+                <div style={{width: '70%', display: 'flex',  flexDirection: 'column', margin: '0 auto', rowGap: '0.5rem', 
+                    alignSelf:'flex-start'}}>
+                    <NameInput type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    <h5>Seattle, WA</h5>
+                    <h6>PhD NEETology</h6>
+                    <h6>University of Pawbark</h6>
+                    <Button type="button" onClick={handleUsername}>Submit</Button>
+                </div>
             </StyledModal>
         </div>
     )
 }
 
-
 const StyledModal = Modal.styled`
-  width: 20rem;
-  height: 20rem;
+  width: 27rem;
+  height: 13rem;
   display: flex;
   align-items: center;
   justify-content: flex-start;
-  flex-direction: column;
+  flex-direction: row;
   background-color: white;
+  font-size: 15px;
+  row-gap: 20px;
+  border: 1px solid #707070;
+`;
+
+const File = styled.input`
+   display: flex;
+   justify-content: flex-start;
+   width: 50%; 
+   margin: 0 auto;
 `;
 
 const Image = styled.div`
     border-radius: 50%;
     background-color: white;
     margin-right: 1rem;
-    width: 50px; 
-    height: 50px;
+    margin-left: 1rem;
+    width: 80%; 
+    height: 50%;
     background-size: cover;
+`;
+
+const ImageModal = styled(Image)`
+    align-self: flex-start;
+    margin-top: 1.5rem;
+`;
+
+const NameInput = styled.input`
+    border: none;
+    border-radius: 5%;
+    outline: none;
+    font-weight: bold;
+    font-size: 30px;
+`;
+
+const Button = styled.button`
+    border: none;
+    border-radius: 5%;
+    margin-right: 3rem;
+`;
+
+const LeftModal = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    height: 100%;
+`;
+
+const EditButton = styled.button`
+    border: none;
+    width: 50%;
+    margin-top: 2.3rem;
+    margin-left: 30%;
 `;
